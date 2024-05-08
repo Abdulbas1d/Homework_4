1a11cb2f9e968a7f526320c0c60aa0eba94a3f52
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-05-08 22:38:02 +0500

    Solution all example

---
+++ js/index.js
@@ -0,0 +1,191 @@
+// 1-Misol
+
+// function sum(a, b) {
+//     return a + b;
+// }
+// console.log(sum(3, 5));
+
+
+
+// 2-Misol
+
+// function minutesToSeconds(minutes) {
+//     return minutes * 60;
+// }
+// console.log(minutesToSeconds(5));
+
+
+
+// 3-Misol
+
+// function nextNumber(integer) {
+//     return integer + 1;
+// }
+// console.log(nextNumber(5));
+
+
+
+// 4-Misol
+
+// function calculateTriangleArea(base, height) {
+//     return (base * height) / 2;
+// }
+
+// let base = 5;
+// let height = 10;
+// let area = calculateTriangleArea(base, height);
+// console.log("The area of the triangle is: " + area);
+
+
+
+
+// 5-Misol
+
+// function calculateAgeInDays(ageInYears) {
+//     return ageInYears * 365;
+// }
+
+// let ageInYears = 25;
+// let ageInDays = calculateAgeInDays(ageInYears);
+// console.log("The age in days is: " + ageInDays);
+
+
+
+
+// 6-Misol
+
+// function calculateAgeInDays(ageInYears) {
+//     return ageInYears * 365;
+// }
+
+
+
+
+// 7-Misol
+
+// function getFirstElement(arr) {
+//     return arr[0];
+// }
+
+
+
+// 8-Misol
+
+
+// function multiplyValues(value1, value2) {
+//     return value1 * value2;
+// }let result = multiplyValues(5, 10);
+// console.log(result);
+
+
+
+// 9-Misol
+
+// function timeToSeconds(hours, minutes, seconds) {
+//     return hours * 3600 + minutes * 60 + seconds;
+// }
+// let totalSeconds = timeToSeconds(2, 30, 15);
+// console.log(totalSeconds);
+
+
+
+// 10-Misol
+
+
+// function longestThirdSide(side1, side2) {
+//     return (side1 + side2) - 1;
+// }
+// let longestSide = longestThirdSide(3, 4);
+// console.log(longestSide); 
+
+
+
+
+// 11-Misol
+
+
+// function findRemainder(num1, num2) {
+//     return num1 % num2;
+// }
+
+
+
+// 12-Misol
+
+// function calculateRectangleArea(height, width) {
+//     return height * width;
+// }
+
+
+
+// 13-Misol
+
+
+// function a(str) {
+//     return "Something" + " " + str;
+// }
+
+
+
+// 14-Misol
+
+// function checkNumber(num) {
+//     return num <= 0;
+// }
+
+
+// 15-Misol
+
+
+// function sumInteriorAngles(n) {
+//     return (n - 2) * 180;
+// }
+
+// console.log(sumInteriorAngles(4));
+// console.log(sumInteriorAngles(6)); 
+
+
+
+
+
+// 16-Misol
+
+
+// function calculateTotalPoints(twoPointGoals, threePointGoals) {
+//     return (2 * twoPointGoals) + (3 * threePointGoals);
+// }
+
+// console.log(calculateTotalPoints(3, 2));
+// console.log(calculateTotalPoints(5, 1));
+
+
+
+
+
+// 17-Misol
+
+
+// function sumLessThan100(num1, num2) {
+//     return (num1 + num2) < 100;
+// }
+
+// console.log(sumLessThan100(50, 40)); 
+// console.log(sumLessThan100(70, 40)); 
+
+
+
+
+// 18-Misol
+
+
+// function checkSum(num1, num2, ...rest) {
+//     if (num1 + num2 >= 100 || rest.length > 0) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+// console.log(checkSum(50, 40));
+// console.log(checkSum(70, 40)); 
+// console.log(checkSum(30, 30, 30)); 
